d698803b6f09caac72fcf7054c8b4062a641f0b5
James Buczkowski <james.sullenriot@gmail.com>
2016-03-19 12:29:00 -0400

    Changd objects to arrays since they are indexed the same, removed indicateStrength strength function

--- js/main.js
+++ js/main.js
@@ -4,61 +4,43 @@ pw_field.onkeyup = function() {
   // get the score from the zxcvbn library
   var result = zxcvbn(pw_field.value);
   var score = result.score;
-  // Call to getMeterPosition function to advance progress bar
-  var highlight = getMeterPosition(score);
+
+  adjustStrengthMeter(score);
+}
+
+/* Update progressbar */
+function adjustStrengthMeter(score) {
+  var color = [
+    "#d50014",
+    "#d50014",
+    "#e8ba00",
+    "#88af0e",
+    "#88af0e"
+  ];
+
+  var strengthText = [
+    "",
+    "Weak",
+    "Moderate",
+    "Strong",
+    "Very Strong"
+  ];
+
+  var percentage = (score * 20) + 20;
+
+  // Call to add text
+  indicateStrength(strengthText[score]);
+
+  // increase and decrease progress bar
+  document.getElementById('progressbar').setAttribute("style",
+                                                        "visibility: visible; width: "
+                                                        + percentage + "%; background-color: "
+                                                        + color[score]);
+
 }
 
 /* Display password strength text */
 function indicateStrength(str) {
   text = document.getElementById("strength-text");
   text.innerHTML = str;
-}
-
-/* Update progressbar */
-function adjustStrengthMeter(score) {
-  var color = {
-    "0": "#d50014",
-    "1": "#d50014",
-    "2": "#e8ba00",
-    "3": "#88af0e",
-    "4": "#88af0e"
-  }
-
-  var strengthText = {
-    "0": "",
-    "1": "Weak",
-    "2": "Moderate",
-    "3": "Strong",
-    "4": "Very Strong"
-  }
-
-  var percentage = (score * 20) + 20;
-
-  indicateStrength(strengthText[score]);
-  document.getElementById('progressbar').style.visibility = "visible";
-  document.getElementById('progressbar').style.width = percentage + "%";
-  document.getElementById('progressbar').style.backgroundColor = color[score];
-}
-
-// none red yellow green
-function getMeterPosition(score) {
-  /* meter position object */
-  var meterPosition = {
-    "0": function() {
-      adjustStrengthMeter(0);
-    },
-    "1": function() {
-      adjustStrengthMeter(1);
-    },
-    "2": function() {
-      adjustStrengthMeter(2);
-    },
-    "3": function() {
-      adjustStrengthMeter(3);
-    },
-    "4": function() {
-      adjustStrengthMeter(4);
-    }
-  }
-  meterPosition[score]();
 }
